615b78781173c4e35d76832e47f91e28f9078c64
Hans Van Moer <hansvm.projects@gmail.com>
2018-01-22 21:23:08 +0100

    initial commit stub project

---
+++ src/game/main.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2017 Hans Van Moer
+ * Licensed under the GNU Public Licence version 3 (https://www.gnu.org/licenses/gpl.html)
+ */
+
+int main(int arg_count, const char **args){
+  return 0;
+}
