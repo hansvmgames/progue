aea156ef733c921b672ce71a3eb70e945a459eb5
Hans Van Moer <hansvm.projects@gmail.com>
2018-01-22 21:36:29 +0100

    added basic logging stub

---
+++ src/utility/Logger.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2017 Hans Van Moer
+ * Licensed under the GNU Public Licence version 3 (https://www.gnu.org/licenses/gpl.html)
+ */
+
+#include "Logger.hpp"
+
+using namespace Game::Log;
+using namespace std;
---
+++ src/utility/Logger.hpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2017 Hans Van Moer
+ * Licensed under the GNU Public Licence version 3 (https://www.gnu.org/licenses/gpl.html)
+ */
+#ifndef GAME_LOGGER_H
+#define GAME_LOGGER_H
+
+namespace Game{
+
+  namespace Log{
+
+    /*
+     * a basic logger
+     */
+    class Logger{};
+    
+  }
+  
+}
+
+#endif
